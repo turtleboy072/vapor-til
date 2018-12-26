0c3f81afe2a0fd5b3374b47a35f0c0ef4acf2043
Jim Allan <jimallan@MACs-MacBook-Pro.local>
2018-12-26 15:02:42 +0530

    first commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,21 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        // 2
+        return try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                // 3
+                return acronym.save(on: req)
+        } }
+}
